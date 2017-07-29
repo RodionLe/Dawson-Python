d7384121434a506ac3d4ba275d90824e63e1cf36
RodionLe <oormorking@mail.ru>
2017-07-29 21:08:03 +0300

    for

---
+++ считалка.py
@@ -0,0 +1,10 @@
+print("Посчитаем: ")
+for i in range(10):
+    print(i, end=" ")
+print("\n\nПосчитаем кратные пяти: ")
+for i in range (0,50,5):
+    print(i, end=" ")
+print("\n\nПосчитаем в обратном порядке: ")
+for i in range(10, 0, -1):
+    print(i, end=" ")
+input("\n\nНажмите enter, чтобы выйти")
