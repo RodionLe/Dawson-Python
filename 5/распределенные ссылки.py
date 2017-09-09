2f9b1a0a9785618cfbce536a20e1d18c1a44d39e
RodionLe <oormorking@mail.ru>
2017-09-09 18:36:05 +0300

    5

--- 5/распределенные ссылки.py
+++ 5/распределенные ссылки.py
@@ -1,13 +1,14 @@
 mike = ["белая рубашка", "комбинезон цвета хаки", "пиджак"]
 mr_dawson = mike
 honey = mike
-print(mike)
-print(mr_dawson)
-print(honey)
+print("mike(original): ", mike)
+print("mr_dawson(copy mike): ", mr_dawson)
+print("honey(copy mike): ", honey)
 honey[2] = "красный свитер"
-print(honey)
-print(mike)
-print(mr_dawson)
+print("change honey")
+print("honey(copy mike): ", honey)
+print("honey(copy mike): ", mike)
+print("honey(copy mike): ", mr_dawson)
 
 mike = ["белая рубашка", "комбинезон цвета хаки", "пиджак"]
 honey = mike[:]
